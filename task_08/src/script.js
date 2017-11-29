30b5b4bd181a8d7d018494b60fd6289b3b0a4a2c
Serafim <SerafimPoch>
2017-11-29 18:47:56 +0200

    Fix animation

--- task_08/src/script.js
+++ task_08/src/script.js
@@ -1,8 +1,11 @@
 const main    = document.querySelector('main')
 const flipper = document.querySelector('.flipper')
-const flip_container = document.querySelector('.flip-container')
 const front   = document.querySelector('.front')
 const cats_0 = document.querySelector('.cat_0')
+
+let memory_values = [];
+let memory_tile = [];
+let cards_flipped = 0
 
 const cards = [
     {id:1,  img:'cats_0'},
@@ -25,26 +28,33 @@ const shuffle = (a) => a.sort(()=> 0.5 - Math.random())
 
 
 const new_board = () => { 
+    cards_flipped = 0;
     let store = shuffle(cards)
          return store.map(x => {
            main.insertAdjacentHTML('beforeend',
-               '<div class="flip-container">\
-               <div class="flipper">\
-               <div onclick="handleClick(this,\''+x.img+'\')" class="front"></div>\
+               '<div class="flip-container ">\
+               <div onclick="handleClick(this,\''+x.img+'\')"  class="flipper">\
+               <div  class="front"></div>\
                <div class="back '+x.img+'"></div>\
                </div>\
                </div>' 
                )    
-            })        
-                 
+            })                     
 }
 
+
 const handleClick= (x,y) => {
-   x.classList.add(y)
+    if(memory_values.length < 2)
+    x.className += ' open'
+ 
+    
+    
 }
 
 new_board()
-
+// const flip_container = document.querySelector('.flip-container')
+// flip_container.classList.add('flipped')
+//          x.classList.add(y)
 
 
 
