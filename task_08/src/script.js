a1c7ac24a09d7d89050dd0094bb24c5bc1fa2e4f
Serafim <SerafimPoch>
2017-11-29 16:48:33 +0200

    Fix turn image

--- task_08/src/script.js
+++ task_08/src/script.js
@@ -1,5 +1,8 @@
-const main = document.querySelector('main')
-
+const main    = document.querySelector('main')
+const flipper = document.querySelector('.flipper')
+const flip_container = document.querySelector('.flip-container')
+const front   = document.querySelector('.front')
+const cats_0 = document.querySelector('.cat_0')
 
 const cards = [
     {id:1,  img:'cats_0'},
@@ -21,25 +24,32 @@ const shuffle = (a) => a.sort(()=> 0.5 - Math.random())
 
 
 
-const random = () => {
-     
+const new_board = () => { 
     let store = shuffle(cards)
          return store.map(x => {
            main.insertAdjacentHTML('beforeend',
                '<div class="flip-container">\
                <div class="flipper">\
-               <div class="front"></div>\
-               <div onclick="handleClick()" class="back '+x.img+'"></div>\
+               <div onclick="handleClick(this,\''+x.img+'\')" class="front"></div>\
+               <div class="back '+x.img+'"></div>\
                </div>\
                </div>' 
                )    
-            })           
+            })        
+                 
 }
 
-random()
-const handleClick = () => {
-   
+const handleClick= (x,y) => {
+   x.classList.add(y)
 }
+
+new_board()
+
+
+
+
+
+
 
 
 
