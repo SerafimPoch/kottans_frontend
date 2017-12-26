a1ec811ceb3c47c0ec1e580e89d74b70c47b34a5
Serafim <SerafimPoch>
2017-12-26 16:10:49 +0200

    Add weather pictures

--- task_09/script.js
+++ task_09/script.js
@@ -1,1 +1,18 @@
 const key = 'e2a4d8b55c5d115b04b00a5b2b95c1c9'
+const button = document.querySelector('.submit')
+const input = document.querySelector('.input')
+let city = '' 
+
+
+
+
+button.addEventListener('click',()=>{  
+    city += input.value
+        input.value = ''
+        
+})
+
+
+
+   
+    
