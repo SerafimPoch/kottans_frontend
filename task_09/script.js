48249606057e766a38d73755bbeacba70cccbdcd
Serafim <SerafimPoch>
2017-12-27 18:16:10 +0200

    Add cards

--- task_09/script.js
+++ task_09/script.js
@@ -1,10 +1,10 @@
 const key = 'e2a4d8b55c5d115b04b00a5b2b95c1c9'
 const button = document.querySelector('.submit')
 const input = document.querySelector('.input')
+const data_1 = document.getElementById('location-1')
 let city = '' 
-
-
-
+const options = { weekday: 'long',  day: 'numeric' }
+    // data_1.innerHTML = new Date('2017-12-27 18:00:00').toLocaleString('en-US', options) // date on Api dt_txt
 
 button.addEventListener('click',()=>{  
     city += input.value
